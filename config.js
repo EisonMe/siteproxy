cf9421852473848110de4e8633e8f1a3e018ca13
EisonMe <mxsyujian@126.com>
2022-11-24 03:37:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy-kohl.vercel.app',
+    serverName: 'siteproxyvercel.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
